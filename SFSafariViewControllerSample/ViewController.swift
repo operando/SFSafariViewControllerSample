3953c4b717606b009401ae089a7910b43fbd63cf
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-08-09 19:20:53 +0900

    Initial Commit

---
+++ SFSafariViewControllerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFSafariViewControllerSample
+//
+//  Created by shinobu okano on 2015/08/09.
+//  Copyright © 2015年 shinobu okano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
